21ed6a6894d2a416abd1ad3c64d33fa9a4c81f22
Marty Cheng <136635641+MartyWYCheng@users.noreply.github.com>
2025-04-16 18:43:41 -0400

    Final Project

--- main.py
+++ main.py
@@ -6,8 +6,36 @@ import os
 
 #############
 import base64
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
 from google import genai
 from google.genai import types
+from google.cloud import texttospeech_v1
+
+
+client2 = texttospeech_v1.TextToSpeechClient()
+
+def sample_synthesize_speech(text=None, ssml=None):
+        input = texttospeech_v1.SynthesisInput()
+        if ssml:
+            input.ssml = ssml
+        else:
+            input.text = text
+
+        voice = texttospeech_v1.VoiceSelectionParams()
+        voice.language_code = "en-UK"
+        # voice.ssml_gender = "MALE"
+
+        audio_config = texttospeech_v1.AudioConfig()
+        audio_config.audio_encoding = "LINEAR16"
+
+        request = texttospeech_v1.SynthesizeSpeechRequest(
+            input=input,
+            voice=voice,
+            audio_config=audio_config,
+        )
+        response = client2.synthesize_speech(request=request)
+        return response.audio_content
 
 
 def generate(filename, prompt):
@@ -15,19 +43,19 @@ def generate(filename, prompt):
         api_key=os.environ.get("GEMINI_API_KEY"),
     )
 
-    files = [
-        # Please ensure that the file is available in local system working direrctory or change the file path.
-        client.files.upload(file=filename),
-    ]
+    files = [client.files.upload(file=f) for f in filename]  # upload each file
+
     model = "gemini-2.5-pro-exp-03-25"
     contents = [
         types.Content(
             role="user",
             parts=[
-                types.Part.from_uri(
-                    file_uri=files[0].uri,
-                    mime_type=files[0].mime_type,
-                ),
+                *[
+                    types.Part.from_uri(
+                        file_uri=f.uri,
+                        mime_type=f.mime_type,
+                    ) for f in files
+                ],
                 types.Part.from_text(text=prompt),
             ],
         ),
@@ -42,15 +70,15 @@ def generate(filename, prompt):
         config=generate_content_config,
     )
     
-    print(response)
-    return response.text
+    print(response.candidates[0].content.parts[0].text)
+    return response.candidates[0].content.parts[0].text
 
 
 app = Flask(__name__)
 
 # Configure upload folder
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'wav'}
+ALLOWED_EXTENSIONS = {'wav','pdf'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -73,37 +101,61 @@ def index():
     files = get_files()
     return render_template('index.html', files=files)
 
+
+
+@app.route('/upload_pdf', methods=['POST'])
+def upload_text():
+    book_file = request.files['pdf_data']
+    filename = 'book.pdf'
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    book_file.save(file_path)
+
+    return redirect('/') #success
+    
+
+
 @app.route('/upload', methods=['POST'])
 def upload_audio():
-    if 'audio_data' not in request.files:
-        flash('No audio data')
-        return redirect(request.url)
-    file = request.files['audio_data']
-    if file.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if file:
-        # filename = secure_filename(file.filename)
-        filename = datetime.now().strftime("%Y%m%d-%I%M%S%p") + '.wav'
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
+    if 'audio_data' in request.files and request.files['audio_data'].filename != '':
+        file = request.files['audio_data']
+        filename = datetime.now().strftime("%Y%m%d-%I%M%S%p")
+        file_path_prompt = os.path.join(app.config['UPLOAD_FOLDER'], filename + '_QUESTION.wav')
+        file.save(file_path_prompt)
+        #prompt_file = Part.from_uri(file_path_prompt, mime_type="audio/wav")
 
+        file_path_book = os.path.join(app.config['UPLOAD_FOLDER'], 'book.pdf')
+        #book_file = Part.from_uri(file_path_book, mime_type="application/pdf")
 
-        #
-        prompt = """
-        Please provide an exact trascript for the audio, followed by sentiment analysis.
+        prompt = "Answer the user's query in the audio file based on the pdf book"
 
-        Your response should follow the format:
+        contents = [file_path_prompt, file_path_book]
+        text = generate(contents, prompt)
 
-        Text: USERS SPEECH TRANSCRIPTION
-
-        Sentiment Analysis: positive|neutral|negative
-        """
-        text = generate(file_path, prompt)
-
-        f = open(file_path+'.txt','w')
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename + '_RESPONSE.wav.txt')
+        f = open(file_path,'w')
+        #print(text)
         f.write(text)
+        f.close()       
+
+        # Save the output as a audio file in the 'tts' directory 
+        wav = sample_synthesize_speech(text)
+
+        # save audio
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename + '_RESPONSE')
+        f = open(file_path + '.wav','wb')
+        f.write(wav)
         f.close()
+
+    else:
+        flash('No data to upload')
+        return redirect(request.url)
+
+    #if file.filename == '':
+    #    flash('No selected file')
+    #    return redirect(request.url)
+
+
+        
         #
 
     return redirect('/') #success
